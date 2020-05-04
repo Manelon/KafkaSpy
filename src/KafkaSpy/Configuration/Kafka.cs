950ddd579eaa1dd1c17cfda41d448e80becd2dac
Manel <manel@manelon.com>
2020-05-04 18:57:49 +0100

    WIP

--- src/KafkaSpy/Configuration/Kafka.cs
+++ src/KafkaSpy/Configuration/Kafka.cs
@@ -5,31 +5,7 @@ namespace KafkaSpy.Configuration
 {
     public class KafkaConfiguration //TODO:Cambiar esto por kafkaClientConfig
     {
-        public String BootstrapServers { get; set; }
-
-        public SecurityProtocol? SecurityProtocol { get; set; } //Plaintext,Ssl,SaslPlaintext,SaslSsl
-        public bool? ApiVersionRequest { get; set; }
-        public string SaslKerberosServiceName { get; set; }
-        public string SaslKerberosKeytab { get; set; }
-        public string SaslKerberosPrincipal { get; set; }
-
-        public ClientConfig BuildClientConfig(){
-            var clientConfig = new ClientConfig()
-            {
-                BootstrapServers = BootstrapServers,
-                SecurityProtocol = SecurityProtocol,
-                ApiVersionRequest = ApiVersionRequest,                
-            };
-            if (!string.IsNullOrWhiteSpace(SaslKerberosServiceName))
-                clientConfig.SaslKerberosServiceName = SaslKerberosServiceName;
-            if (!string.IsNullOrWhiteSpace(SaslKerberosKeytab))
-                clientConfig.SaslKerberosKeytab= SaslKerberosKeytab;
-            if (!string.IsNullOrWhiteSpace(SaslKerberosPrincipal))
-                clientConfig.SaslKerberosPrincipal= SaslKerberosPrincipal;
-            
-            return clientConfig;
-        }
-        
+        public ClientConfig ClientConfig {get;set;}   
     }   
 
     
